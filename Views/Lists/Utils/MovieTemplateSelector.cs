8afbdcd1b4000844c53345e0cbbe04df2c34c25b
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-08-01 16:05:19 -0300

    secao 11 concluida

---
+++ Views/Lists/Utils/MovieTemplateSelector.cs
@@ -0,0 +1,17 @@
+﻿using AppMauiGallery.Views.Lists.Models;
+
+namespace AppMauiGallery.Views.Lists.Utils;
+
+public class MovieTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate TemplateDefault { get; set; }
+
+    public DataTemplate TemplateLongMovie { get; set; }
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        Movie movie = (Movie)item;
+
+        return (movie.Duration.Hours > 1) ? TemplateLongMovie : TemplateDefault;
+    }
+}
